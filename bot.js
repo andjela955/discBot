de2b4bdfc2fa47d836c0089494555034e0dcc6cb
aarsic <andjelaarsic955@gmail.com>
2019-03-20 22:38:34 +0100

    test

--- bot.js
+++ bot.js
@@ -19,21 +19,19 @@ bot.on('ready', function (evt) {
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '$') {
+    if (message.substring(0, 1) == '~') {
         var args = message.substring(1).split(' ');
         var cmd = (args[0]).toLowerCase();
        
         args = args.splice(1);
 
         switch(cmd) {
-            // !ping
             case 'hi':
                 bot.sendMessage({
                     to: channelID,
-                    message: 'Hi ' + user + '. I hope you are having a d'
+                    message: 'Hi ' + user + '. I hope you are having a good day! :relaxed:'
                 });
             break;
-            // Just add any case commands if you want to..
          }
      }
 });
